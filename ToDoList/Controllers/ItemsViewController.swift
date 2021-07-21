c10b6aad469244fa1dca0bad5017c0352a7a5eb4
kenjiWuJian <wujian1404@gmail.com>
2021-07-21 08:25:14 +0800

    Search Bar

--- ToDoList/Controllers/ItemsViewController.swift
+++ ToDoList/Controllers/ItemsViewController.swift
@@ -75,3 +75,21 @@ class ItemsViewController: UITableViewController {
     }
 }
 
+extension ItemsViewController: UISearchBarDelegate{
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request:NSFetchRequest<Item> = Item.fetchRequest()
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@",searchBar.text!)
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        loadItems(with: request)
+    }
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0{
+            loadItems()
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+            
+        }
+    }
+}
+
